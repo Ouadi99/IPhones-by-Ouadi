75b6b065ce2bc7e9f5c950466f3e21b6d20004d8
Ouadi Yassine (s107854) <yassine.ouadi@student.ap.be>
2022-11-17 17:09:04 +0100

    Begonnen aan deelopdracht 3

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,57 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import { container, nav, navLinks, navLinkItem, navLinkText } from './layout.module.css'
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
 
-const Layout = ({ pageTitle, children }) => {
-  return (
-    <div className={container}>
-      <title>{pageTitle}</title>
-      <nav className={nav}>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/">
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/about">
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
-  )
-}
-
-export default Layout
+ import * as React from "react"
+ import PropTypes from "prop-types"
+ import { useStaticQuery, graphql } from "gatsby"
+ 
+ import Header from "./header"
+ import "./layout.css"
+ 
+ const Layout = ({ children }) => {
+   const data = useStaticQuery(graphql`
+     query SiteTitleQuery {
+       site {
+         siteMetadata {
+           title
+         }
+       }
+     }
+   `)
+ 
+   return (
+     <>
+       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+       <div
+         style={{
+           margin: `0 auto`,
+           maxWidth: `var(--size-content)`,
+           padding: `var(--size-gutter)`,
+         }}
+       >
+         <main>{children}</main>
+         <footer
+           style={{
+             marginTop: `var(--space-5)`,
+             fontSize: `var(--font-sm)`,
+           }}
+         >
+           © {new Date().getFullYear()} &middot; Built with
+           {` `}
+           <a href="https://www.gatsbyjs.com">Gatsby</a>
+         </footer>
+       </div>
+     </>
+   )
+ }
+ 
+ Layout.propTypes = {
+   children: PropTypes.node.isRequired,
+ }
+ 
+ export default Layout
+ 
